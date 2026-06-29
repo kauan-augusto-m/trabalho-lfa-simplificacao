46fadb2021775dbb164bc4968ac3d317ca46af0f
kauan-augusto-m <24401518+kauan-augusto-m@users.noreply.github.com>
2026-06-29 17:47:43 -0300

    implementei o verifica inuteis

--- src/verificaInuteis.py
+++ src/verificaInuteis.py
@@ -1,1 +1,60 @@
+def calculaGeradores(Gramatica):
 
+    Geradores = []
+    GeradoresTemp = []
+
+    
+
+    for cabeca, corpo in Gramatica.items():
+
+        for elemento in corpo:
+
+            cont = 0
+
+            for letra in elemento:
+
+                # terminal
+                if letra not in Gramatica:
+                    cont += 1
+
+            if cont == len(elemento):
+
+                if cabeca not in Geradores:
+                    Geradores.append(cabeca)
+
+    tam = len(Geradores)
+
+    
+
+    while True:
+
+        for cabeca, corpo in Gramatica.items():
+
+            for elemento in corpo:
+
+                cont = 0
+
+                for letra in elemento:
+
+                    if letra not in Gramatica:
+                        cont += 1
+
+                    elif letra in Geradores:
+                        cont += 1
+
+                if cont == len(elemento):
+
+                    if cabeca not in Geradores and cabeca not in GeradoresTemp:
+
+                        GeradoresTemp.append(cabeca)
+
+        Geradores.extend(GeradoresTemp)
+
+        if len(Geradores) == tam:
+            break
+
+        else:
+            tam = len(Geradores)
+            GeradoresTemp.clear()
+
+    return Geradores
